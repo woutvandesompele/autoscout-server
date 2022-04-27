bd32f09a2fa520b19196888aab94cf6ff1d0ed10
Wout Van de Sompele <wout.van.de.sompele@student.howest.be>
2022-04-27 19:38:58 +0200

    added stuff

--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env('URL', 'http://localhost:1337')
 });
